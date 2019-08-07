0db400ff11846f38b192527c72ba010463264f1d
DFrost55555 <darren.frost55555@outlook.com>
2019-08-07 16:52:49 +0100

    Update settings.py

--- django_azure_demo/settings.py
+++ django_azure_demo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')d26$oxat5lst9(nujr^$y!oehx&v_h4he)8@r@vtxl+i0wpwf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
